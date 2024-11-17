4c356b2f337e8ebd7551efe6e9e9af3ee5072151
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-11-17 19:52:12 +0530

    update 9 files

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:free_scroll/free_scroll.dart';
 
+class FreeScrollScreen extends StatelessWidget {
+  FreeScrollScreen({super.key});
+
+  // Instantiate the FreeScrollManager
+  final FreeScrollManager freeScrollManager = FreeScrollManager();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Free Scroll")),
+      body: Stack(
+        children: [
+          FreeScroll(
+            key: freeScrollManager.key,
+            children: [
+              for (int i = 0; i <= 100; i++)
+                Container(
+                  width: 150,
+                  height: 150,
+                  color: Colors.blue[100 * ((i % 9) + 1)],
+                  alignment: Alignment.center,
+                  child: Text('Item $i', style: const TextStyle(fontSize: 16)),
+                )
+            ],
+          ),
+          Positioned(
+            bottom: 16,
+            right: 16,
+            child: FloatingActionButton(
+              onPressed: () {
+                // If you want to reset the view when clicking the button, use this method
+                freeScrollManager.resetView();
+              },
+              child: const Icon(Icons.refresh),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,30 +61,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Freeform Scroll and Zoom Example',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const ExampleScreen(),
-    );
-  }
-}
-
-class ExampleScreen extends StatelessWidget {
-  const ExampleScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Freeform Scroll and Zoom Example")),
-      body: FreeScroll(
-        children: [
-          for (int i = 0; i <= 100; i++)
-            Container(
-              width: 150,
-              height: 150,
-              color: Colors.blue[100 * ((i % 9) + 1)],
-              alignment: Alignment.center,
-              child: Text('Item $i', style: const TextStyle(fontSize: 16)),
-            )
-        ],
-      ),
+      home: FreeScrollScreen(),
     );
   }
 }
--- lib/free_scroll.dart
+++ lib/free_scroll.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 
 /// A widget that enables freeform scrolling and zooming of widgets within a constrained area.
 ///
 /// The `FreeScroll` widget allows users to zoom in/out and scroll the content freely. The widget
 /// supports pinch-to-zoom gestures as well as pointer scroll events for zooming. It also provides
 /// customizable padding, spacing, and other layout configurations.
-class FreeScroll extends HookWidget {
+class FreeScroll extends StatefulWidget {
   /// The background color of the scrollable area.
   final Color? backgroundColor;
 
@@ -32,7 +31,7 @@ class FreeScroll extends HookWidget {
   /// The spacing between items vertically.
   final double? runSpacing;
 
-  /// pass children to show your screen
+  /// Children widgets to display in the scrollable area.
   final List<Widget> children;
 
   /// Creates a new [FreeScroll] widget.
@@ -50,81 +49,84 @@ class FreeScroll extends HookWidget {
   });
 
   @override
+  FreeScrollState createState() => FreeScrollState();
+}
+
+class FreeScrollState extends State<FreeScroll> {
+  late double _scale;
+  late Offset _offset;
+  late Offset _startingFocalPoint;
+  late Offset _previousOffset;
+
+  @override
+  void initState() {
+    super.initState();
+    _scale = 1.0;
+    _offset = Offset.zero;
+  }
+
+  void resetView() {
+    setState(() {
+      _scale = 1.0;
+      _offset = Offset.zero;
+    });
+  }
+
+  /// Handles the start of the scale gesture.
+  void onScaleStart(ScaleStartDetails details) {
+    _startingFocalPoint = details.focalPoint;
+    _previousOffset = _offset;
+  }
+
+  /// Updates the scale and offset as the scale gesture progresses.
+  void onScaleUpdate(ScaleUpdateDetails details) {
+    setState(() {
+      _scale = (_scale * details.scale).clamp(widget.minScale ?? 1.0, widget.maxScale ?? 3.0);
+      final Offset offsetDelta = details.focalPoint - _startingFocalPoint;
+      _offset = _previousOffset + offsetDelta;
+    });
+  }
+
+  /// Handles pointer scroll events to zoom in or out.
+  void onPointerSignal(PointerSignalEvent event) {
+    if (event is PointerScrollEvent) {
+      setState(() {
+        _scale = (_scale + (event.scrollDelta.dy > 0 ? -0.1 : 0.1)).clamp(widget.minScale ?? 1.0, widget.maxScale ?? 3.0);
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final scale = useState(1.0); // Keeps track of the current scale factor.
-    final offset =
-        useState(Offset.zero); // Keeps track of the current offset (position).
-    final startingFocalPoint =
-        useState(Offset.zero); // The starting point of the scale gesture.
-    final previousOffset =
-        useState(Offset.zero); // The offset before scaling begins.
-
-    /// Handles the start of the scale gesture.
-    void onScaleStart(ScaleStartDetails details) {
-      startingFocalPoint.value = details.focalPoint;
-      previousOffset.value = offset.value;
-    }
-
-    /// Updates the scale and offset as the scale gesture progresses.
-    void onScaleUpdate(ScaleUpdateDetails details) {
-      scale.value =
-          (scale.value * details.scale).clamp(minScale ?? 1.0, maxScale ?? 3.0);
-
-      final Offset offsetDelta = details.focalPoint - startingFocalPoint.value;
-      offset.value = previousOffset.value + offsetDelta;
-    }
-
-    /// Handles pointer scroll events to zoom in or out.
-    void onPointerSignal(PointerSignalEvent event) {
-      if (event is PointerScrollEvent) {
-        scale.value = (scale.value + (event.scrollDelta.dy > 0 ? -0.1 : 0.1))
-            .clamp(minScale ?? 1.0, maxScale ?? 3.0);
-      }
-    }
-
     return Listener(
       onPointerSignal: onPointerSignal, // Listens for pointer scroll events.
       child: GestureDetector(
-        onScaleStart:
-            onScaleStart, // Triggers on the start of the scaling gesture.
+        onScaleStart: onScaleStart, // Triggers on the start of the scaling gesture.
         onScaleUpdate: onScaleUpdate, // Triggers when the scale is updated.
         child: Container(
-          width: width ??
-              double
-                  .infinity, // Use provided width or take the max available width.
-          height: height ??
-              double
-                  .infinity, // Use provided height or take the max available height.
-          color: backgroundColor ??
-              Colors.white, // Background color, defaults to light grey.
+          width: widget.width ?? double.infinity, // Use provided width or take the max available width.
+          height: widget.height ?? double.infinity, // Use provided height or take the max available height.
+          color: widget.backgroundColor ?? Colors.white, // Background color, defaults to white.
           child: Padding(
-            padding: padding ??
-                const EdgeInsets.all(
-                    20.0), // Use provided padding or default to 20.0.
+            padding: widget.padding ?? const EdgeInsets.all(20.0), // Use provided padding or default to 20.0.
             child: ClipRect(
               // Clips the content to avoid overflowing.
               child: Transform(
-                alignment:
-                    Alignment.center, // The center will be used for zooming.
+                alignment: Alignment.center, // The center will be used for zooming.
                 transform: Matrix4.identity()
-                  ..translate(offset.value.dx,
-                      offset.value.dy) // Apply translation (scrolling).
-                  ..scale(scale.value), // Apply scaling (zooming).
+                  ..translate(_offset.dx, _offset.dy) // Apply translation (scrolling).
+                  ..scale(_scale), // Apply scaling (zooming).
                 child: LayoutBuilder(
                   builder: (context, constraints) {
                     return ConstrainedBox(
                       constraints: BoxConstraints(
-                        minWidth: constraints
-                            .maxWidth, // Ensure content fills the available width.
-                        minHeight: constraints
-                            .maxHeight, // Ensure content fills the available height.
+                        minWidth: constraints.maxWidth, // Ensure content fills the available width.
+                        minHeight: constraints.maxHeight, // Ensure content fills the available height.
                       ),
                       child: Wrap(
-                        spacing: spacing ??
-                            15.0, // Horizontal spacing between items.
-                        runSpacing: runSpacing ??
-                            15.0, // Vertical spacing between items.
-                        children: children,
+                        spacing: widget.spacing ?? 15.0, // Horizontal spacing between items.
+                        runSpacing: widget.runSpacing ?? 15.0, // Vertical spacing between items.
+                        children: widget.children,
                       ),
                     );
                   },
@@ -135,5 +137,22 @@ class FreeScroll extends HookWidget {
         ),
       ),
     );
+  }
+}
+
+/// A helper class to manage the `FreeScroll` widget's key and actions.
+class FreeScrollManager {
+  /// A [GlobalKey] that uniquely identifies the `FreeScroll` widget and its state.
+  final GlobalKey<FreeScrollState> _freeScrollKey = GlobalKey<FreeScrollState>();
+
+  /// Getter for the [GlobalKey] to access the `FreeScroll` widget's state.
+  GlobalKey<FreeScrollState> get key => _freeScrollKey;
+
+  /// Resets the view of the `FreeScroll` widget.
+  ///
+  /// This method calls the `resetView` method on the current state of the
+  /// `FreeScroll` widget, resetting its position and zoom level to the default.
+  void resetView() {
+    _freeScrollKey.currentState?.resetView();
   }
 }
